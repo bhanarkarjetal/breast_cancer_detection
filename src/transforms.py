4a8ac68bcd174ca6d762cdd6990ec85e12d5b672
bhanarkarjetal <jetal123@Jetal.localdomain>
2025-09-11 14:25:56 +0200

    Modify transforms.py file added dtypes of input parameters for data validation modifed code for better readability

--- src/transforms.py
+++ src/transforms.py
@@ -1,52 +1,55 @@
 import torch
 from torchvision.transforms import v2
+from typing import Optional, Tuple, List, Dict
 
-class data_transform():
-    def __init__ (self, resize = None, 
-                   random_crop = None,
-                   normalize = None,
-                   random_horizontal_flip = None,
-                   random_vertical_flip = None,
-                   random_photometric_distort = None
-                  ):
+class DataTransform():
+    def __init__ (self, resize: Optional[Tuple[int, int]] = None, 
+                   random_crop: Optional[Tuple[int, int]] = None,
+                   normalize: Optional[Dict[str, List[float]]] = None,
+                   random_horizontal_flip: Optional[float] = None,
+                   random_vertical_flip: Optional[float] = None,
+                   random_photometric_distort: Optional[Dict[str, List[float]]] = None,
+                   default_resize = (64, 64),
+                   default_normalize_mean = [0.485, 0.456, 0.406],
+                   default_normalize_std = [0.229, 0.224, 0.225]):
+        
         self.resize = resize
         self.random_crop = random_crop
         self.normalize = normalize
         self.random_horizontal_flip = random_horizontal_flip
         self.random_vertical_flip = random_vertical_flip
         self.random_photometric_distort = random_photometric_distort
+        self.default_resize = default_resize
+        self.default_normalize_mean = default_normalize_mean
+        self.default_normalize_std = default_normalize_std
 
     def transform(self):
         transform_list = []
-        if self.resize:
+        if self.resize is not None:
             transform_list.append(v2.Resize(self.resize))
         else:
-            transform_list.appen(v2.Resize(64,64))
+            transform_list.append(v2.Resize((self.default_resize)))
 
-        try:
-            if self.random_crop:
-                transform_list.append(v2.RandomCrop(self.random_crop))
-        except Exception:
-            print('Crop size greater than resize size')
+        if self.random_crop is not None:
+            transform_list.append(v2.RandomCrop(self.random_crop))
 
-        if self.random_horizontal_flip:
-            transform_list.append(v2.RandomHorizontalFlip())
+        if self.random_horizontal_flip is True:
+            transform_list.append(v2.RandomHorizontalFlip(self.random_horizontal_flip))
 
-        if self.random_vertical_flip:
-            transform_list.append(v2.RandomVerticalFlip())
+        if self.random_vertical_flip is True:
+            transform_list.append(v2.RandomVerticalFlip(self.random_vertical_flip))
 
-        if self.random_photometric_distort:
-            transform_list.append(v2.RandomPhotometricDistort())
+        if self.random_photometric_distort is True:
+            transform_list.append(v2.RandomPhotometricDistort(self.random_photometric_distort))
 
         transform_list.append(v2.ToImage())
         transform_list.append(v2.ToDtype(torch.float, scale = True))
 
-        if self.normalize:
-            transform_list.append(v2.Normalize())
+        if self.normalize is not None:
+            transform_list.append(v2.Normalize(self.normalize['mean'], 
+                                               self.normalize['std']))
         else:
-            transform_list.append(v2.Normalize(mean=[0.485, 0.456, 0.406], 
-                                               std=[0.229, 0.224, 0.225]))
-        
-
+            transform_list.append(v2.Normalize(mean=self.default_normalize_mean, 
+                                               std=self.default_normalize_std))
         
         return v2.Compose(transform_list)
