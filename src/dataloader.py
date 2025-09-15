e547467e95247567abd940f7df1fab43339450fd
bhanarkarjetal <jetal123@Jetal.localdomain>
2025-09-15 13:18:58 +0200

    Remove class from the script. Add docstring to get_data_loader function

--- src/dataloader.py
+++ src/dataloader.py
@@ -2,26 +2,40 @@ from torch.utils.data import DataLoader
 from create_dataset import BreastCancerDataset
 from typing import Optional, Callable
 
-class DataLoaderWrapper(DataLoader):
-    def __init__(self, annotation_file: str, 
-                 transforms: Optional[Callable] = None, 
-                 root_dir: str = '',
-                 batch_size: int = None, 
-                 shuffle: bool = True):
-        
-        self.annotation_file = annotation_file
-        self.transforms = transforms
-        self.root_dir = root_dir
-        self.batch_size = batch_size
-        self.shuffle = shuffle
+def get_data_loader(
+    annotation_file: str,
+    root_dir: str,
+    transforms: Optional[Callable] = None,
+    batch_size: int = 32,
+    shuffle: bool = True,
+    num_workers: int = 2
+):
+    """
+    Utility function to create a DataLoader for the BreastCancerDataset.
+    
+    Args:
+        annotation_file (str): Path to the CSV file with annotations.
+        root_dir (str): Directory with all the images.
+        transforms (callable, optional): Optional transformations to be applied on a sample.
+        batch_size (int, optional): Number of samples per batch to load. Default is 32.
+        shuffle (bool, optional): Set to True to have the data reshuffled at every epoch. Default is True.
+        num_workers (int, optional): Number of subprocesses to use for data loading. Default is 2.
 
-    def load_data(self):
-        dataset = BreastCancerDataset(csv_file = self.annotation_file,
-                                     root_dir = self.root_dir,
-                                     transform = self.transforms)
-        
-        data_loader = DataLoader(dataset, 
-                                 batch_size = self.batch_size, 
-                                 shuffle = self.shuffle)
-        
-        return data_loader
+    Returns:
+        DataLoader: A PyTorch DataLoader instance.
+    """
+
+    dataset = BreastCancerDataset(
+        csv_file=annotation_file,
+        root_dir=root_dir,
+        transform=transforms
+    )
+
+    data_loader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        num_workers=num_workers
+    )
+
+    return data_loader
