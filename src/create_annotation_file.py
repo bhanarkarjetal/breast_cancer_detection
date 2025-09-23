2371d6c2d01eba586ee8d09cf03a1486e7586a39
bhanarkarjetal <jetal123@Jetal.localdomain>
2025-09-23 17:14:31 +0200

    Changed args name for better understanding

--- src/create_annotation_file.py
+++ src/create_annotation_file.py
@@ -6,11 +6,11 @@ import pandas as pd
 
 # create a class for creating annotation file
 class CreateAnnotationFile:
-    def __init__(self, dataset_dir_name, subset_name):
+    def __init__(self, dataset_dir_name, split_dataset_name):
         self.dataset_dir_name = dataset_dir_name
-        self.subset_name = subset_name
+        self.split_dataset_name = split_dataset_name
 
-    def create_annotation(self):
+    def create_annotation_df(self):
         """
         Create annotation file for the given subset (train, test, valid)
 
@@ -20,19 +20,19 @@ class CreateAnnotationFile:
         image_list = []
         labels = []
 
-        subset_path = os.path.join(self.dataset_dir_name, self.subset_name)
-        if not os.path.exists(subset_path):
-            raise ValueError(f"Subset path {subset_path} does not exist.")
+        split_dataset_path = os.path.join(self.dataset_dir_name, self.split_dataset_name)
 
-        for path in os.listdir(subset_path):
-            images = glob.glob(f"{subset_path}/{path}/*jpg")
+        if not os.path.exists(split_dataset_path):
+            raise ValueError(f"Subset path {split_dataset_path} does not exist.")
+
+        for cls in os.listdir(split_dataset_path):            
+            images = glob.glob(os.path.join(split_dataset_path, cls, "*.jpg"))
             image_list.extend(images)
-            labels.extend([path] * len(images))
+            labels.extend([cls] * len(images))
 
-        data = {"image_path": image_list, "label": labels}
-        self.annotation_df = pd.DataFrame(data)
-
-        return self.annotation_df
+        df = pd.DataFrame({"image_path": image_list, "label": labels})
+        self.annotation_df = df
+        return df
 
     def save_annotation_file(self, df: pd.DataFrame, destination_path: str):
         """
@@ -44,11 +44,10 @@ class CreateAnnotationFile:
         if not os.path.exists(destination_path):
             os.makedirs(destination_path)
 
-        # save annotation file in current directory
-        df.to_csv(
-            f"{destination_path}/{self.subset_name}_annotations.csv",
-            index=False,
-        )
+        file_name = f"{self.split_dataset_name}_annotation.csv"
+        file_path = os.path.join(destination_path, file_name)
+        df.to_csv(file_path, index=False)
+        return file_name, file_path
 
 
 if __name__ == "__main__":
@@ -57,9 +56,9 @@ if __name__ == "__main__":
     destination_path = "test_annotation"
 
     annotation_creator = CreateAnnotationFile(dataset_dir_name, subset_name)
-    annotation_df = annotation_creator.create_annotation()
+    annotation_df = annotation_creator.create_annotation_df()
     annotation_creator.save_annotation_file(annotation_df, destination_path)
 
     print(
-        f"Annotation file for {subset_name} subset saved to {destination_path}/{subset_name}_annotations.csv"
+        f"Annotation file for {subset_name} subset saved to {destination_path}/{subset_name}_annotation.csv"
     )
